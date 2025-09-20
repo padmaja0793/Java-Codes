471c2e9b149be5e5c2c6e827023ea13e2698af8b
padmaja0793 <padmaja7210@gmail.com>
2025-09-20 05:29:51 +0100

    Java

---
+++ Array/ArrayString.java
@@ -0,0 +1,10 @@
+class ArrayString {
+    public static void main(String args[]) {
+        String[] fruits = {"Apple", "Banana", "Grapes", "Mango"};
+
+        System.out.println("Fruits in array: ");
+        for (int i = 0; i <fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+    }
+}
